6896888c1a57286ee05870c98c01dab2c1d79096
imbowbow42 <phamgbao42@gmail.com>
2026-03-22 16:30:26 +0700

    CORS Adjustment for custom domain

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 // 1. Security & Parsing Middleware (Runs on every request)
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:3300', 'https://kind-ocean-06f7e6b00.4.azurestaticapps.net'],
+    origin: ['http://localhost:3300', 'https://kind-ocean-06f7e6b00.4.azurestaticapps.net', 'https://login-api.jettpham.cloud'],
     credentials: true
 }));
 app.use(rateLimit({
